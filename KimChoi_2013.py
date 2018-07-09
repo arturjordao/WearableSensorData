15599ce3f4e146e0b5abc645e0330e940cbb0363
arturjordao <arturjordao@users.noreply.github.com>
2018-07-09 11:08:13 -0300

    Update KimChoi_2013.py

--- KimChoi_2013.py
+++ KimChoi_2013.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     if (len(sys.argv) > 1):
         data_input_file = sys.argv[1]
     else:
-        data_input_file = 'E:/datasets/sensors/TemporalWindow/CV_0.5/UTD-MHAD2_1s.npz'
+        data_input_file = 'data/LOSO/MHEALTH.npz'
 
     tmp = np.load(data_input_file)
     X = tmp['X']
