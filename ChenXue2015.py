8dbde2ab5d99ced4f3b7856f6f8f5538a1cd2b53
arturjordao <arturjordao@users.noreply.github.com>
2018-07-09 11:05:44 -0300

    Update ChenXue2015.py

--- ChenXue2015.py
+++ ChenXue2015.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     if (len(sys.argv) > 1):
         data_input_file = sys.argv[1]
     else:
-        data_input_file = '//storage.vpr.dcc.ufmg.br/home/projects/sensor2.0/SavedFeatures/CV_0.5/MHEALTH_5s.npz'
+        data_input_file = 'data/LOSO/MHEALTH.npz'
 
     tmp = np.load(data_input_file)
     X = tmp['X']
