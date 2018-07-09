6e26156833959ffabdcb5effc42fc881d688e54d
arturjordao <arturjordao@users.noreply.github.com>
2018-07-09 11:08:34 -0300

    Update Kimetal_2012.py

--- Kimetal_2012.py
+++ Kimetal_2012.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     if (len(sys.argv) > 1):
         data_input_file = sys.argv[1]
     else:
-        data_input_file = 'C:/Users/Artur Jordao/Desktop/UFMG/Projeto/sensors2.0/SavedFeatures/UTD-MHAD2_1s.npz'
+        data_input_file = 'data/LOSO/MHEALTH.npz'
 
     tmp = np.load(data_input_file)
     X = tmp['X']
