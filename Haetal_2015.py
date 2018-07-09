3723da176a2d1c1f8ff0dd5a7d3064e3bc5487ad
arturjordao <arturjordao@users.noreply.github.com>
2018-07-09 11:07:05 -0300

    Update Haetal_2015.py

--- Haetal_2015.py
+++ Haetal_2015.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     if (len(sys.argv) > 1):
         data_input_file = sys.argv[1]
     else:
-        data_input_file = 'C:/Users/ARTUR/Desktop/Residuals/PAMAP2P_tete.npz'
+        data_input_file = 'data/LOSO/MHEALTH.npz'
 
     tmp = np.load(data_input_file)
     X = tmp['X']
